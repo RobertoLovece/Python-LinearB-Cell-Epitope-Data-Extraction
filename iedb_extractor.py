cd4c1ef749e0663fab4db810ac1ef78233b3fd74
Roberto <rlovece@hotmail.co.uk>
2020-11-14 20:03:06 +0000

    made process epitope function, defined needed parameters paths

--- iedb_extractor.py
+++ iedb_extractor.py
@@ -6,24 +6,44 @@ import xml.etree.ElementTree as et
 
 root = et.parse('285.xml').getroot()
 
+path = "{http://www.iedb.org/schema/CurationSchema}"
+
 # find out if it's a Linear B-Cell epitopes
 def check_linear_bcell_epitopes():
 
-    path_to_bcell = "{http://www.iedb.org/schema/CurationSchema}Reference/{http://www.iedb.org/schema/CurationSchema}Epitopes/{http://www.iedb.org/schema/CurationSchema}Epitope/{http://www.iedb.org/schema/CurationSchema}Assays/{http://www.iedb.org/schema/CurationSchema}BCell"
-    path_to_linear = "{http://www.iedb.org/schema/CurationSchema}Reference/{http://www.iedb.org/schema/CurationSchema}Epitopes/{http://www.iedb.org/schema/CurationSchema}Epitope/{http://www.iedb.org/schema/CurationSchema}EpitopeStructure/{http://www.iedb.org/schema/CurationSchema}FragmentOfANaturalSequenceMolecule"
+    bcell_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path + "Assays/"+ path +"BCell"
+    linear_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule"
 
     bcell_array = []
     linear_array = []
 
-    for bcell in root.findall(path_to_bcell):
+    for bcell in root.findall(bcell_path):
         bcell_array.append(bcell)
 
-    for linear in root.findall(path_to_linear):
+    for linear in root.findall(linear_path):
         linear_array.append(linear)
 
     if (len(linear_array) != 0 and len(bcell_array) != 0):
         return True
     else:
         return False
+
+def process_epitopes():
+
+    # the paths to all the parameter that need processing
+    pubmed_id_path = path +"Reference/"+ path + "Article/" + path + "PubmedId"
+    year_path = path +"Reference/"+ path + "Article/" + path + "ArticleYear"
+    epit_name_path = path +"Reference/"+ path + "Epitopes/" + path + "Epitope/" + "EpitopeName"
+    epitope_id_path = path +"Reference/"+ path + "Epitopes/" + path + "Epitope/" + "EpitopeId"
+    evid_code_path = path +"Reference/"+ path + "Epitopes/" + path + "Epitope/" + "EpitopeEvidenceCode"
+    epit_struc_def_path = path +"Reference/"+ path + "Epitopes/" + path + "Epitope/" + "EpitopeStructureDefines"
+    sourceorg_id_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"SourceOrganismId"
+    protein_id_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"SourceMolecule/"+ path +"GenBankId"
+    epit_seq_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"LinearSequence"
+    start_pos_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"StartingPosition"
+    end_pos_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope/"+ path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"EndingPosition"
+
+
+
 
 print(check_linear_bcell_epitopes())

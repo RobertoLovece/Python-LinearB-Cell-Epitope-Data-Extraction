07c3e4cc3ddd48e07f12d408d9f145d00c0c7192
Roberto <rlovece@hotmail.co.uk>
2020-11-30 18:37:37 +0000

    made it so all epitopes are processed in each xml file not just 1

--- iedb_extractor.py
+++ iedb_extractor.py
@@ -25,28 +25,41 @@ def main():
             root = et.parse(xml).getroot()
             print(xml)
             if (check_linear_bcell_epitopes(root) == True):
+
+                epitope_path = path +"Reference/"+ path +"Epitopes/"+ path +"Epitope"
                 final_array = None
-                # only getting one epitope from each file
 
                 # get the article information first
                 article_array = get_article_information(root)
-                epitope_array = process_epitope(root)
-                assay_array = process_assays(root)
+                # go through all the epitopes
 
-                # combines the epitope and assay data into one array
-                final_array = np.append(article_array, epitope_array)
-                final_array = np.append(final_array, assay_array)
+                n = 0
+                for epitope in root.findall(epitope_path):
+                    print(n)
+                    n= n+1
+                    epitope_array = process_epitope_v2(epitope)
 
-                # 15 parameters need to get passed to the DataFrame
-                df = pd.DataFrame([final_array],columns=["pubmed_id","year","epit_name","epitope_id","evid_code","epit_struc_def","sourceOrg_id","protein_id","epit_seq","start_pos","end_pos","host_id","bcell_id","class","assay_type"])
-                # print(df)
+                    if (check_assay(epitope) == True):
+                        assay_array = process_assays_v2(epitope)
+
+                        # combines the epitope and assay data into one array
+                        final_array = np.append(article_array, epitope_array)
+                        final_array = np.append(final_array, assay_array)
+
+                        # 15 parameters need to get passed to the DataFrame
+                        df = pd.DataFrame([final_array],columns=["pubmed_id","year","epit_name","epitope_id","evid_code","epit_struc_def","sourceOrg_id","protein_id","epit_seq","start_pos","end_pos","host_id","bcell_id","class","assay_type"])
+
+                    else:
+                        final_array = np.append(article_array, epitope_array)
+                        # 11 parameters need to get passed to the DataFrame
+                        df = pd.DataFrame([final_array],columns=["pubmed_id","year","epit_name","epitope_id","evid_code","epit_struc_def","sourceOrg_id","protein_id","epit_seq","start_pos","end_pos"])
 
                 # if the file is empty adds a header otherwise appends it to csv file
-                if (file_empty == True):
-                    df.to_csv('output.csv', mode='a',index=False)
-                    file_empty = False
-                else:
-                    df.to_csv('output.csv', mode='a',index=False, header=False)
+                    if (file_empty == True):
+                        df.to_csv('output.csv', mode='a',index=False)
+                        file_empty = False
+                    else:
+                        df.to_csv('output.csv', mode='a',index=False, header=False)
 
         except et.ParseError:
             print("Parse error in "+ xml +",invalid xml format")
@@ -85,6 +98,59 @@ def get_article_information(root):
         article_array = np.array([pubmed_id, year])
 
         return article_array
+
+def process_epitope_v2(epitope):
+    # the paths to all the parameters that need processing
+    # part of the the epitope section
+    epit_name_path = path +"EpitopeName"
+    epitope_id_path = path +"EpitopeId"
+    evid_code_path = path +"EpitopeEvidenceCode"
+    epit_struc_def_path = path +"EpitopeStructureDefines"
+    sourceorg_id_path = path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"SourceOrganismId"
+    protein_id_path = path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"SourceMolecule/"+ path +"GenBankId"
+    epit_seq_path = path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"LinearSequence"
+    start_pos_path = path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"StartingPosition"
+    end_pos_path = path +"EpitopeStructure/"+ path +"FragmentOfANaturalSequenceMolecule/"+ path +"EndingPosition"
+
+    # processes the parameter that are required
+    epit_name = process_epitope_data(epitope, epit_name_path)
+    epitope_id = process_epitope_data(epitope, epitope_id_path)
+    evid_code = process_epitope_data(epitope, evid_code_path)
+    epit_struc_def = process_epitope_data(epitope, epit_struc_def_path)
+    sourceorg_id = process_epitope_data(epitope, sourceorg_id_path)
+    protein_id = process_epitope_data(epitope, protein_id_path)
+    epit_seq = process_epitope_data(epitope, epit_seq_path)
+
+    # If the starting or end positions tags are empty we check another location
+    if(epitope.find(start_pos_path) != None):
+        start_pos = epitope.find(start_pos_path).text
+    else:
+        start_pos_path = path +"ReferenceStartingPosition"
+        if(epitope.find(start_pos_path) != None):
+            start_pos = epitope.find(start_pos_path).text
+        else:
+            start_pos = "NA"
+
+    if(epitope.find(end_pos_path) != None):
+        end_pos = epitope.find(end_pos_path).text
+    else:
+        end_pos_path = path +"ReferenceEndingPosition"
+        if(epitope.find(end_pos_path) != None):
+            end_pos = epitope.find(end_pos_path).text
+        else:
+            end_pos = "NA"
+
+    epitope_array = np.array([epit_name, epitope_id, evid_code, epit_struc_def, sourceorg_id, protein_id, epit_seq, start_pos, end_pos])
+
+    return epitope_array
+
+def check_assay(epitope):
+    assay_path = path + "Assays"
+
+    if(epitope.find(assay_path) == None):
+        return False
+    else:
+        return True
 
 def process_epitope(root):
 
@@ -139,6 +205,43 @@ def process_epitope_data(root, path):
     else:
         processed = "NA"
     return processed
+
+def process_assays_v2(epitope):
+    # paths for the assay parameters that need processing
+    host_id_path = path +"Assays/"+ path +"BCell/"+ path +"Immunization/"+ path +"HostOrganism/"+ path +"OrganismId"
+    bcell_id_path = path +"Assays/"+ path +"BCell/"+ path +"BCellId"
+    class_path = path +"Assays/"+ path +"BCell/"+ path +"AssayInformation/"+ path +"QualitativeMeasurement"
+    assay_type_path = path +"Assays/"+ path +"BCell/"+ path +"AssayInformation/"+ path +"AssayTypeId"
+
+    # finds all data from each assay parameter as each assay can have multiple tags
+    # then appends it to a string so it's outputted correctly
+    loop_list = []
+
+    for host_id in epitope.findall(host_id_path):
+        loop_list.append('"'+ host_id.text +'"')
+        host_ids = ','.join(loop_list)
+
+    loop_list.clear()
+
+    for bcell_id in epitope.findall(bcell_id_path):
+        loop_list.append('"'+ bcell_id.text +'"')
+        bcell_ids = ','.join(loop_list)
+
+    loop_list.clear()
+
+    for assay_class in epitope.findall(class_path):
+        loop_list.append('"'+ assay_class.text +'"')
+        classes = ','.join(loop_list)
+
+    loop_list.clear()
+
+    for assay_type in epitope.findall(assay_type_path):
+        loop_list.append('"'+ assay_type.text +'"')
+        assay_types = ','.join(loop_list)
+
+    assay_array = np.array([host_ids, bcell_ids, classes, assay_types])
+
+    return assay_array
 
 # process the data from each assay section of the epitope
 def process_assays(root):
